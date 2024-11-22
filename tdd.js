9f954c4c687c2f3d73cd267e5e60a70ae5851324
morganeliza <maccarthymorgan1@gmail.com>
2024-11-21 21:51:49 -0600

    first comit

---
+++ tdd.js
@@ -0,0 +1,40 @@
+//Vending Machine
+
+//Functional Tests
+
+//Expect price to be displayed when user enters item
+
+//Expect when the user inserts cash currency it will be USD otherwise machine does not accept currency
+//currency.given == currency.accepted
+
+//Expect when the user taps or swipes Visa or Mastercard credit card machine accepts form of payment
+
+//Ensure when user pays to return the difference between the given amount and price amount
+//givenAmount - priceAmount
+
+//Ensure machine mechanism properly releases item and drops item down to the dispensary area 
+//item.selected == item.released
+
+//Ensure after transaction is complete to prompt the suer if they want a receipt, yes or no? 
+//If yes, print receipt of transaction
+
+//Unit Tests
+// Expect concatOdds([91, 37, 23], [55, 11, 61, 1, 4, 12, 6])
+//result in [1, 11, 23, 37, 91]
+
+// Expect concatOdds([101, 101, 28], [66, 13, 13, 1, 4, 10, 7])
+//result in [1, 7, 13, 101]
+
+// Expect concatOdds([77, 2, 27], [66, 14, 13, 13, 4, 19, 7])
+//result in [7, 13, 19, 27, 77]
+
+//Expect (2 * 3) to be 6
+//Expect (4 * 17) to be 68
+//Expect (5.137 * 88) to be 452.056
+
+
+
+
+
+
+
